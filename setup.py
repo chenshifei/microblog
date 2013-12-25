61289ccd5d3447df6f00119bc1147d384e525f2d
John Chen <chenshifei.j@gmail.com>
2013-12-25 16:04:38 +0800

    correct format

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-import os, subprocess, sys
+import os
+import subprocess
+import sys
+
 subprocess.call(['python', 'virtualenv.py', 'flask'])
 if sys.platform == 'win32':
     bin = 'Scripts'
@@ -9,13 +12,26 @@ subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask<0.10'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-login'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-openid'])
 if sys.platform == 'win32':
-    subprocess.call([os.path.join('flask', bin, 'pip'), 'install', '--no-deps', 'lamson', 'chardet', 'flask-mail'])
+    subprocess.call(
+        [
+            os.path.join('flask', bin, 'pip'),
+            'install',
+            '--no-deps',
+            'lamson',
+            'chardet',
+            'flask-mail'
+        ])
 else:
-    subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-mail'])
-subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy==0.7.9'])
-subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-sqlalchemy'])
-subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy-migrate'])
-subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-whooshalchemy'])
+    subprocess.call(
+        [os.path.join('flask', bin, 'pip'), 'install', 'flask-mail'])
+subprocess.call(
+    [os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy==0.7.9'])
+subprocess.call(
+    [os.path.join('flask', bin, 'pip'), 'install', 'flask-sqlalchemy'])
+subprocess.call(
+    [os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy-migrate'])
+subprocess.call(
+    [os.path.join('flask', bin, 'pip'), 'install', 'flask-whooshalchemy'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-wtf'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-babel'])
 subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flup'])
