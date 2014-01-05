315104bd98c1fb8541162cd23e9f376a842415bd
John Chen <chenshifei.j@gmail.com>
2014-01-05 18:50:45 +0800

    release 0.13

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from flask.ext.mail import Mail
+from momentjs import momentjs
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME,\
     MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = momentjs
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
